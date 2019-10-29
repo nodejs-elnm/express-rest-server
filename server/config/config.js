5d67336bb8953759d4395e78707608c80a0209a1
Erik Navarrete <leonavas1995@gmail.com>
2019-10-29 22:53:29 +0100

    Hide user and pass

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafeDB';
 } else {
-    urlDB = 'mongodb+srv://elnm-admin:wOUhrqf6sjVs9xOL@cluster-elnm-yan3y.mongodb.net/CAFE?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
